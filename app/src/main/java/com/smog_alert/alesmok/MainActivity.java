c408e66f18f699aa9c93ef6b6e5f7396ba5c9e82
SynKsiendza <synksiendza@proton.me>
2023-03-13 22:54:15 +0100

    Build 02 - v0.1.0a
    Project Init

--- app/src/main/java/com/smog_alert/alesmok/MainActivity.java
+++ app/src/main/java/com/smog_alert/alesmok/MainActivity.java
@@ -1,45 +1,20 @@
 package com.smog_alert.alesmok;
 
-import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.tabs.TabLayout;
-
-import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-
-import com.smog_alert.alesmok.ui.main.SectionsPagerAdapter;
-import com.smog_alert.alesmok.databinding.ActivityMainBinding;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = binding.viewPager;
-        viewPager.setAdapter(sectionsPagerAdapter);
-        TabLayout tabs = binding.tabs;
-        tabs.setupWithViewPager(viewPager);
-        FloatingActionButton fab = binding.fab;
-
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setContentView(R.layout.activity_main);
+        WebView mWebView = findViewById(R.id.wvPortal);
+        mWebView.loadUrl("file:///android_asset/www/index.html");
+        WebSettings mWebSettings = mWebView.getSettings();
+        mWebSettings.setJavaScriptEnabled(true);
     }
 }
